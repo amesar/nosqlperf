5f1a4c905d2a76f791f7cbc345b5f43eee351bbb
Andre <amesar77@hotmail.com>
2013-04-30 22:45:18 -0400

    first major push

---
+++ shell.sh
@@ -0,0 +1,25 @@
+
+#*************************************************************************
+#
+# shell.sh - Run NoSQL KeyValue Shell program.
+#
+# Options:
+#   --maxToDisplay $NUM_CHARS - maximum number of value chars to display
+#
+#*************************************************************************
+
+. ./common.env
+
+PGM=com.amm.nosql.cli.KeyValueShell
+
+
+if [ $# -gt 0 ] ; then
+  hosts=$1
+  shift `expr $OPTIND - 1`
+  fi
+
+echo ">> hosts=$hosts"
+echo ">> PROPS=$PROPS"
+
+java $PROPS -cp $CPATH $PGM $* | tee log.txt
+
